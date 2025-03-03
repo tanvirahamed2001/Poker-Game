e7cab54d686c4cee916d840404b6e1ae8a0bdf37
Brandon Greene <brandino941@gmail.com>
2025-03-03 12:05:19 -0700

    added basic card [client + server] / deck [server] classes

---
+++ server/games/card_based/card.java
@@ -0,0 +1,28 @@
+public class Card {
+    
+    public enum Suit {HEARTS, DIAMOND, CLUBS, SPADES}
+
+    public enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
+
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit get_suit() {
+        return this.suit;
+    }
+
+    public Rank get_rank() {
+        return this.rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+}
---
+++ server/games/card_based/deck.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Deck {
+
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+
+        for(Card.Suit suit : Card.Suit.values()) {
+            for(Card.Rank rank : Card.Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+
+        shuffle()
+    }
+
+    public void shuffle() {
+        Collections.shuffle(card);
+    }
+
+    public Card deal_card() {
+        if(cards.isEmpty()) {
+            throw new IllegalStateException("No Cards Left In Deck!");
+        }
+        return cards.remove(cards.size() - 1);
+    }
+}
