4a5f2af2b4fc64b1276cfb0aa48a446feb7b24b0
Larryman555 <larryman555@gmail.com>
2025-03-03 20:15:53 -0700

    small name change

--- server/ServerMain.java
+++ server/ServerMain.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 
 public class ServerMain  {
 	private static ExecutorService gamepool = Executors.newFixedThreadPool(100);//arbitrary number, can be changed as needed
-	private static HashMap<Integer, ArrayList<Socket>> active_games = new HashMap<Integer, ArrayList<Socket>>(); //needs to be a map to maintain unique game IDs, ArrayList contains a list of the sockets currently waiting for that game to start
+	private static HashMap<Integer, ArrayList<Socket>> matching_games = new HashMap<Integer, ArrayList<Socket>>(); //needs to be a map to maintain unique game IDs, ArrayList contains a list of the sockets currently waiting for that game to start
 	public static final int maxplayers = 2;//also arbitrary
 	private static int id = 1;//keeps track of the current unique id that will be assigned to the next game
 	public static void main(String[] args) {
@@ -15,14 +15,14 @@ public class ServerMain  {
 		thread.start();
 	}
 	public static HashMap<Integer, ArrayList<Socket>> getGames() {
-		return ServerMain.active_games;
+		return ServerMain.matching_games;
 	}
 	//returning  -1 means the room was full, otherwise it got added successfully
 	public static int waitforGame(int key, Socket socket) {
-		if(active_games.get(key).size() < maxplayers) {
-			active_games.get(key).add(socket);
-			if(active_games.get(key).size() == maxplayers) {//start the game cause there's max players
-				gamepool.submit(new ServerTable(active_games.get(key)));
+		if(matching_games.get(key).size() < maxplayers) {
+			matching_games.get(key).add(socket);
+			if(matching_games.get(key).size() == maxplayers) {//start the game cause there's max players
+				gamepool.submit(new ServerTable(matching_games.get(key)));
 			}
 			return 0;
 		}
@@ -31,7 +31,7 @@ public class ServerMain  {
 		}
 	}
 	public static synchronized int addNewGame() {
-		active_games.put(id, new ArrayList<Socket>());
+		matching_games.put(id, new ArrayList<Socket>());
 		id++; //if this was IRL this would be a potential risk due to integer overflows
 		return id;
 	}
