df2b91306454c36ebe51050375d46658f3b0f763
Brandon Greene <brandino941@gmail.com>
2025-03-09 11:04:35 -0600

    added Player object to server side for future

---
+++ server/games/Player.java
@@ -0,0 +1,62 @@
+// Basic information for a "Player"
+// Might need to expand upon in the future
+// Will work for what we need currently
+
+package games;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+
+    private String name;
+    private double funds;
+
+    /**
+     * Creates a new Player object from client information.
+     * @param name
+     * @param funds
+     */
+    public Player(String name, double funds) {
+        this.name = name;
+        this.funds = funds;
+    }
+
+    /**
+     * Gets Player name
+     * @return
+     */
+    public String get_name() {
+        return this.name;
+    }
+
+    /**
+     * Get Player funds
+     * @return
+     */
+    public double view_funds() {
+        return this.funds;
+    }
+
+    /**
+     * Player deposits more funds
+     * @param amount
+     */
+    public void deposit_funds(double amount) {
+        this.funds += amount;
+    }
+
+    /**
+     * Bets a specific amount and takes it out of the Players amount
+     * @param bet
+     * @return
+     */
+    public double bet_amount(double bet) {
+        if(bet <= this.funds && bet != 0) {
+            this.funds = bet;
+            return bet;
+        }
+        return 0;
+        //TODO: Error out with something
+    }
+
+}
