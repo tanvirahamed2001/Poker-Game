c3dfa764341f1d911296a3d704fea564bb6a6bcb
Brandon Greene <brandino941@gmail.com>
2025-03-09 11:03:52 -0600

    added comments to Player

--- client/games/Player.java
+++ client/games/Player.java
@@ -11,23 +11,45 @@ public class Player implements Serializable {
     private String name;
     private double funds;
 
+    /**
+     * Creates a new Player object from client information.
+     * @param name
+     * @param funds
+     */
     public Player(String name, double funds) {
         this.name = name;
         this.funds = funds;
     }
 
+    /**
+     * Gets Player name
+     * @return
+     */
     public String get_name() {
         return this.name;
     }
 
+    /**
+     * Get Player funds
+     * @return
+     */
     public double view_funds() {
         return this.funds;
     }
 
+    /**
+     * Player deposits more funds
+     * @param amount
+     */
     public void deposit_funds(double amount) {
         this.funds += amount;
     }
 
+    /**
+     * Bets a specific amount and takes it out of the Players amount
+     * @param bet
+     * @return
+     */
     public double bet_amount(double bet) {
         if(bet <= this.funds && bet != 0) {
             this.funds = bet;
