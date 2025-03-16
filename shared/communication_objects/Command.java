ec3f5ef8c718cb296a8b0c53b4e152ddd543c429
Brandon Greene <brandino941@gmail.com>
2025-03-16 14:51:58 -0600

    progress: gameover command class

--- shared/communication_objects/Command.java
+++ shared/communication_objects/Command.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Command implements Serializable {
 
-    public enum Type {GAMES_LIST, PLAYER_INFO, TURN_CHOICE, GAME_CHOICE, MESSAGE}
+    public enum Type {GAMES_LIST, PLAYER_INFO, TURN_CHOICE, GAME_CHOICE, MESSAGE, GAME_OVER}
 
     private Type type;
     private Object payload;
