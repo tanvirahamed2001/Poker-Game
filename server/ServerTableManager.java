e73066f3c09971f9a1686ad8414ed83f2fa7a6e8
tanvirahamed2001 <127472391+tanvirahamed2001@users.noreply.github.com>
2025-03-16 00:43:30 -0600

    Adjusting and removing socket dependency

--- server/ServerTableManager.java
+++ server/ServerTableManager.java
@@ -1,74 +1,69 @@
-//takes threads that are accepted and shows them the options for tables
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import shared.Player;
 
-//TODO: Rewrite how we handle assigning players and sockets
+// Handles assigning players to tables and manages socket communication
 public class ServerTableManager implements Runnable {
-	Socket conn;
-	BufferedWriter out;
-	BufferedReader in;
-	ObjectInputStream obj_in;
-	ObjectOutputStream obj_out;
-	static HashMap<Integer, ArrayList<Player>> games = ServerMain.getGames();
-	Player player;
+    private Socket conn;
+    private BufferedWriter out;
+    private BufferedReader in;
+    private static HashMap<Integer, ArrayList<Player>> games = ServerMain.getGames();
+    private Player player;
 
-	public ServerTableManager(Socket newconn, Player player) {
-		this.conn = newconn;
-		this.player = player;
-		this.player.socket = newconn;
-		try {
-			out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public ServerTableManager(Socket newConn, Player player) {
+        this.conn = newConn;
+        this.player = player;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run() {
+        int game = getInput();
+        if (game > 0) {
+            ServerMain.waitforGame(game, player);
+        } else if (game == 0) {
+            game = ServerMain.addNewGame();
+            ServerMain.waitforGame(game, player);
+        } else {
+            // Handle error case
+            System.err.println("Invalid input or error occurred.");
+        }
+    }
 
-	public void run() {
-		int game = getInput();
-		if(game > 0) {
-			ServerMain.waitforGame(game, player);
-		}
-		else if(game == 0) {
-			game = ServerMain.addNewGame();
-			ServerMain.waitforGame(game, player);
-		}
-		else {
-			//freak out cause there's an error, I dunno, TODO
-		}
-	}
-	private int getInput() {//gets the input from the client, a non-zero number should be the game id, 0 should create a new game, and -1 means error
-		try {
-			String message ="Games Available:\n";
-			String response;
-			Integer[] keys = games.keySet().toArray(new Integer[]{}); //gets the keys within the map
-			for(int i = 0; i < games.size(); i++) {
-				message += "Game " + keys[i] + ": " + games.get(keys[i]).size() + "/" + ServerMain.maxplayers + " Players" + "\n"; //Should be something like "Game X: X/X Players"
-			}
-			message += "Please input the game number you would like to join, or type 'new' to create a new game\nDone\n"; //use Done to signal to the client that it's done sending stuff
-			out.write(message);
-			out.flush();
-			response = in.readLine();//should be a number or "new"
-			try {
-				int table = Integer.parseInt(response);
-				return table;
-			} catch(NumberFormatException e) {
-				if(response.equals("new")) {
-					return 0;
-				}
-				else {
-					out.write("Invalid Input! Please Try Again\nDone\n");
-					return getInput();
-				}	
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return -1;//shouldn't reach here unless something went wrong, probably an IOException
-	}
+    private int getInput() {
+        try {
+            String message = "Games Available:\n";
+            String response;
+            Integer[] keys = games.keySet().toArray(new Integer[]{}); // Get the keys within the map
+            for (int i = 0; i < games.size(); i++) {
+                message += "Game " + keys[i] + ": " + games.get(keys[i]).size() + "/" + ServerMain.maxplayers + " Players\n";
+            }
+            message += "Please input the game number you would like to join, or type 'new' to create a new game\nDone\n";
+            out.write(message);
+            out.flush();
+            response = in.readLine(); // Read client response
+
+            try {
+                int table = Integer.parseInt(response);
+                return table;
+            } catch (NumberFormatException e) {
+                if (response.equalsIgnoreCase("new")) {
+                    return 0; // Create a new game
+                } else {
+                    out.write("Invalid Input! Please Try Again\nDone\n");
+                    out.flush();
+                    return getInput(); // Retry input
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1; // Error case
+    }
 }
