7dcd0157986c303a789f03f60bb71e21a7b2e148
Brandon Greene <brandino941@gmail.com>
2025-04-10 13:48:22 -0600

    abandoned: client reconnecting a little to complicated to get done in the timeframe we have

---
+++ server/TableCodeGenerator.java
@@ -0,0 +1,15 @@
+import java.security.SecureRandom;
+
+public class TableCodeGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int CODE_LENGTH = 6;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateTableCode() {
+        StringBuilder sb = new StringBuilder(CODE_LENGTH);
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+}
